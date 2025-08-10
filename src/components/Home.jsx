1a4bdd0af9f51119c52e18882d9d8c47f350a423
salonichauhan1101 <salonichauhan1101@gmail.com>
2025-08-10 19:37:43 -0400

    Initial commit - Portfolio website

---
+++ src/components/Home.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import HeroImage from "../assets/heroImage.png";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { Link } from "react-scroll";
+
+const Home = () => {
+  return (
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full">
+          <h4 className="text-6xl sm:text-4xl font-bold text-white">
+            Full Stack Developer | Cloud Engineer | Machine Learning Enthusiast
+          </h4>
+          <p className="text-gray-500 py-4 max-w-md">
+            I specialize in building scalable, high-performance applications and delivering data-driven solutions. With over 3 years of professional experience and a Master’s in Computer Science (in progress) from Northeastern University, I bring expertise in Java, Python, SQL, and modern web technologies like React, Node.js, and Tailwind CSS — all backed by strong cloud skills in AWS and Azure.
+          </p>
+
+          <div>
+            <Link
+              to="portfolio"
+              smooth
+              duration={500}
+              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+            >
+              View my work
+              <span className="group-hover:rotate-90 duration-300">
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
+              </span>
+            </Link>
+          </div>
+        </div>
+
+        <div>
+          <img
+            src={HeroImage}
+            alt="my profile"
+            className="rounded-2xl mx-auto w-2/3 md:w-full"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
