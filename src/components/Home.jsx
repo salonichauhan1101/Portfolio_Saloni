ab351c193dff858025de61642d97d5376103088f
salonichauhan1101 <salonichauhan1101@gmail.com>
2025-08-28 17:49:37 -0400

    Update Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -134,6 +134,7 @@ import HeroImage from "../assets/heroImage.png";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-scroll";
 
+
 const Home = () => {
   return (
     <section
